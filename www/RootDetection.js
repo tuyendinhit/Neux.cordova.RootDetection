ac6c98a45ccc70eaf529a3a8ae48bd23eac3d788
Bryant Wu <davidzxc12@gmail.com>
2019-11-13 16:45:04 +0800

    fix parameter

--- www/RootDetection.js
+++ www/RootDetection.js
@@ -1,7 +1,7 @@
 var exec = require('cordova/exec');
-var detect = function (arg0, success, error) {
-    console.warn('root detection!', arg0, success, error);
-    exec(success, error, 'RootDetection', 'detect', [arg0]);
+var detect = function (success, error) {
+    console.warn('root detection!', success, error);
+    exec(success, error, 'RootDetection', 'detect', []);
 };
 
 module.exports = {
