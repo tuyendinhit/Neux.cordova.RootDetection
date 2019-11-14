a42368e33bf8340cf1d67ae3dafdc9d9be1a70f3
HsuanYuLin <kevinlin1168@gmail.com>
2019-11-14 20:58:20 +0800

    add android root detection

--- src/android/RootDetection.java
+++ src/android/RootDetection.java
@@ -9,17 +9,72 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.*;
+import java.io.*;
+
+import android.content.pm.PackageManager;
+import android.app.ActivityManager;
+
 import android.Manifest;
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class RootDetection extends CordovaPlugin {
+    private static final String[] cheakPackagesList = {
+            "com.thirdparty.superuser", "eu.chainfire.supersu", "com.noshufou.android.su",
+            "com.koushikdutta.superuser", "com.zachspong.temprootremovejb", "com.ramdroid.appquarantine", "com.topjohnwu.magisk"
+    };
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("detect")) {
+            this.rootDetect(callbackContext);
+            return true;
+        }
 
-        return true;
+        return false;
+    }
+
+    private void rootDetect(CallbackContext callbackContext) {
+        if(this.checkRootByFile() || this.checkRootByPackage()) {
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, true);
+            pluginResult.setKeepCallback(true);
+            callbackContext.sendPluginResult(pluginResult);
+        } else {
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, false);
+            pluginResult.setKeepCallback(true);
+            callbackContext.sendPluginResult(pluginResult);
+        }
+    }
+
+    private boolean checkRootByFile() {
+        for(String pathDir : System.getenv("PATH").split(":")){
+            if(new File(pathDir, "su").exists()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private  boolean checkRootByPackage() {
+        for (String packageUri : cheakPackagesList) {
+            if (this.appInstalledOrNot(packageUri)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean appInstalledOrNot(String uri) {
+        PackageManager pm = this.cordova.getActivity().getPackageManager();
+        try {
+            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+        }
+
+        return false;
     }
 
 }
